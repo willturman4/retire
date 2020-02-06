335a0e440a4a4b70e8c0807e116d7b5e8f1c91eb
willturman4 <59977936+willturman4@users.noreply.github.com>
2020-02-06 15:51:41 -0600

    Add files via upload

--- retire.c
+++ retire.c
@@ -9,19 +9,27 @@
 
 int main(void) {
 	
-	double intitalBalence;
-	double montlyCont;
+	double initialBalence;
+	double monthlyCont;
 	double rateOfReturn;
 	double rateOfInfaltion;
-	double yearToRetirement;
-	float monthToRetirement;
-	float interest;
-	float inflation;
+	int    yearToRetirement;
+	int    monthToRetirement;
+	double interest;
+	double interestRate;
+	double monthlyInterestRate;
+	double totalInterest; 
+	double balence;
+	double roundedInterest;
+	double roundedBalence;
+	double roundedTotalInterest;
+	int i=1;
 	
-	printf("Enter intial starting balance:\n"); 
-	scanf("%lf", &intialBalence);
 	
-	printf('Enter monthly contribution amount\n);
+	printf("Enter initial starting balance:\n"); 
+	scanf("%lf", &initialBalence);
+	
+	printf("Enter monthly contribution amount\n");
 	scanf("%lf", &monthlyCont);
 	
 	printf("Enter annual rate of return\n");
@@ -33,9 +41,36 @@ int main(void) {
 	printf("Enter number of years until retirement\n");
 	scanf("%lf", &yearToRetirement);
 	
-	monthToRetirement= yearToRetirement/12 ;
-	interest= intialBalence*rateOfReturn ;
-	inflation=((1+rateOfReturn)/(1+rateOfInfaltion))-1 ;
+	monthToRetirement= yearToRetirement*12 ;
+	
+	interestRate=((1+rateOfReturn)/(1+rateOfInfaltion))-1 ;
+	
+	monthlyInterestRate=interestRate/12 ;
+	
+	printf("Month    Interest     Balance");
+	for(i=1; i<=monthToRetirement; i++ ) {
+		interest=monthlyInterestRate*initialBalence;
+		roundedInterest= round(interest*100)/100;
+		
+		balence=interest+monthlyCont+initialBalence; 
+		roundedBalence= round(balence*100)/100;
+		
+		totalInterest = totalInterest + interest;
+		roundedTotalInterest= round(totalInterest*100)/100;
+		
+		printf("%d %lf %lf\n", monthToRetirement, interest, balence);
+	}
+	printf("Total Interest Earned: $ %lf \n", totalInterest);
+	printf("Total Nest Eggs: $ %lf ", balence);
+	
+	return 0;
+}
+	
+	
+	
+	
+	
+	
 	
 	
 	
