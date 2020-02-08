f70eb02643ec8edbcb6ea9ebc0b6f63a2788d9ad
davidho503 <59979210+davidho503@users.noreply.github.com>
2020-02-07 23:56:11 -0600

    new changes Not fully working

--- retire.c
+++ retire.c
@@ -1,6 +1,6 @@
 /*
   Programmer's Name:William Turman, David Ho
-  NUID: 05032615, 
+  NUID: 05032615,
   Date: 1/28/2020
   Description of the program: Calculating a 401k
 */
@@ -8,7 +8,7 @@
 #include<math.h>
 
 int main(void) {
-	
+
 	double initialBalence;
 	double monthlyCont;
 	double rateOfReturn;
@@ -18,61 +18,67 @@ int main(void) {
 	double interest;
 	double interestRate;
 	double monthlyInterestRate;
-	double totalInterest; 
+	double totalInterest;
 	double balence;
 	double roundedInterest;
 	double roundedBalence;
 	double roundedTotalInterest;
-	int i=1;
-	
-	
-	printf("Enter initial starting balance:\n"); 
+	double interest2;
+	double balence2;
+	int i;
+
+
+	printf("Enter initial starting balance:\n");
 	scanf("%lf", &initialBalence);
-	
+
 	printf("Enter monthly contribution amount\n");
 	scanf("%lf", &monthlyCont);
-	
+
 	printf("Enter annual rate of return\n");
 	scanf("%lf", &rateOfReturn);
 
 	printf("Enter annual rate of inflation\n");
 	scanf("%lf", &rateOfInfaltion);
-	
+
 	printf("Enter number of years until retirement\n");
-	scanf("%lf", &yearToRetirement);
-	
+	scanf("%d", &yearToRetirement);
+
 	monthToRetirement= yearToRetirement*12 ;
-	
+
 	interestRate=((1+rateOfReturn)/(1+rateOfInfaltion))-1 ;
-	
+
 	monthlyInterestRate=interestRate/12 ;
-	
-	printf("Month    Interest     Balance");
-	for(i=1; i<=monthToRetirement; i++ ) {
-		interest=monthlyInterestRate*initialBalence;
-		roundedInterest= round(interest*100)/100;
-		
-		balence=interest+monthlyCont+initialBalence; 
-		roundedBalence= round(balence*100)/100;
-		
-		totalInterest = totalInterest + interest;
-		roundedTotalInterest= round(totalInterest*100)/100;
-		
-		printf("%d %lf %lf\n", monthToRetirement, interest, balence);
+
+	interest=initialBalence;
+	balence=monthlyCont+initialBalence;
+	interest2 = interest;
+	balence2 = balence;
+
+	if(monthlyCont>=18500){
+		printf("Illegal: monthly contributions exceed $18,500 annual limit.");
+		return 0;
 	}
-	printf("Total Interest Earned: $ %lf \n", totalInterest);
-	printf("Total Nest Eggs: $ %lf ", balence);
-	
+		if(initialBalence<0){
+			printf("Illegal: illegal input");
+			return 0;
+		}
+	printf("Month    Interest     Balance\n");
+	for(i=1; i<=monthToRetirement; i++ ) {
+		interest2 = interest2;
+		interest2=interest2*monthlyInterestRate;
+
+		roundedInterest= round(interest2*100)/100;
+
+		balence2 = interest2 + balence2;
+		roundedBalence= round(balence2*100)/100;
+
+		totalInterest = totalInterest + interest2;
+		roundedTotalInterest= round(totalInterest*100)/100;
+
+		printf("%d %.2f %.2f\n", i, roundedInterest, roundedBalence);
+	}
+	printf("Total Interest Earned: $ %.2f \n", totalInterest);
+	printf("Total Nest Eggs: $ %.2f ", balence2);
+
 	return 0;
 }
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
